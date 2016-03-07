56dea799abaf76d48276595eec65d05572e19ca2
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2016-03-07 17:33:10 -0400

    Added extract_examples_from method to Crawler class
    extract_examples_from extracts examples from the <ul> tags that are
    inside a provided Nokogiri::XML::Element object which is a <li> tag

--- crawler.rb
+++ crawler.rb
@@ -13,25 +13,28 @@ class Crawler
     @search = @page.forms.first
   end
 
-  def crawl a_word
+  def crawl a_word, with_examples = false
     #gets definitions for the given word
+    #The with_examples argument especifies if examples from the
+    #a_page should be extracted too.
 
     #sets the value of the search form value property (which name is search)
     #to the word paset to the method
     @search.search = a_word
     #submit the form to get the new Page
     @page = @search.submit
-    extract_definitions_from @page
+    extract_definitions_from @page, with_examples
   end
 
-  def extract_definitions_from a_page
-    #extracts definitions from the given Mechanize::Page object
+  def extract_definitions_from a_page, with_examples = false
+    #Extracts definitions from the given Mechanize::Page object
+    #The with_examples argument especifies if examples from the
+    #a_page should be extracted too.
 
     word = Hash.new
     definitions = Hash.new
-    #examples = Hash.new
+    examples = Hash.new
     definition_counter = 1
-    #examples_counter = 1
 
     #the xpath query that is passed to the search method says the following:
     #get all the h3 with a span with a class named mw-headline as children
@@ -60,18 +63,23 @@ class Crawler
         #takes the text that is before the first \n character.
         begin
           definitions[definition_counter] = /^(.*?)\n/.match(currently_selected_item.text)[0]
+          examples[definition_counter] = extract_examples_from currently_selected_item if with_examples
+
         rescue NoMethodError #there are no examples in for this specific definition, so the \n charecter magic is not needed
           definitions[definition_counter] = currently_selected_item.text
+          examples[definition_counter] = extract_examples_from currently_selected_item if with_examples
         end
 
         definition_counter += 1 #increase definition_counter to select the next definition <li>
       end
       definition_counter = 1 #resets definition_counter to select definitions for another kind
-      word[heading.text.downcase.gsub(' ','_')] = {definitions: definitions}
+      word[heading.text.downcase.gsub(' ','_')] = {definitions: definitions, examples: examples}
       definitions = {} #clears the definitions hash to saves definitions for another kind
+      examples = {} #clears the examples hash to saves examples for another definition
     end
     #A Hash containing the difinitions of a Word. This hash have the structure
-    #{kind: definitions: 1: 'adsf', 2: 'adsfasf', 3: 'etc'}
+    #{:kind => :definitions => {1=> 'adsf', 2=> 'adsfasf', 3=> 'etc', :examples => 1=> list of examples}}
+    #:examples will be an empty hash in case with_examples is set to false
     word
   end
 
@@ -100,9 +108,30 @@ class Crawler
       "no match"
     end
   end
+
+  def extract_examples_from current_li_element
+    #Extracs definition from a provided Nokogiri::XML::Element which is a <li>
+    #and return an Array of all the examples that are within the provided
+    #li element
+    #
+    #Is wise to remember that all definitions are in <ol> tags and that all
+    #examples are in <ul> tags as children of the <li> tags from the <ol> tags
+    example_number = 1
+    examples = Array.new
+
+    #If there examples in current_li_element (in case the name is not descriptive
+    #enough current_li_element is a Nokogiri::XML::Element that represents a <li> tag)
+    #the while loop will execute and extract all examples.
+    while current_li_element.at("ul/li[#{example_number}]") do
+      current_example = current_li_element.at("ul/li[#{example_number}]")
+      examples.push current_example.text
+      example_number += 1
+    end
+    examples
+  end
 end
 
 
 c = Crawler.new 'https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil'
-p c.crawl 'fasse'
+p c.crawl 'maison'
 
